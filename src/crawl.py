1a013bf81adb45a157382cbc824198b03ec9fcae
= <chengtsung_liu@yahoo.com>
2017-05-05 13:11:52 +0800

    Initialize the main cralwer file

---
+++ src/crawl.py
@@ -0,0 +1,13 @@
+'''
+Start from the homepage
+	crawl all urls for a single country page, store the urls in a list
+crawl countries: 
+	crawl information for each single country using the url stored
+	crawl all urls for a single city page in the country, store the urls in a list
+crawl cities:
+	crawl information for each single city in a country using the url stored
+	crawl all urls for a single attraction page in the country, store the url in a list
+crawl attractions:
+	crawl information for each single attraction in a city using the url stored
+
+'''	
